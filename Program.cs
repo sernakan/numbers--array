c577d2d98f1affa4521f21d89dcb3e801023eab9
Natalya <can-natasha@yzndex.ru>
2023-03-10 20:50:53 +0500

    Массив вещественных чисел

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿///задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+//m = 3, n = 4.
+
+//0,5 7 -2 -0,2
+
+//1 -3,3 8 -9,9
+
+//8 7,8 -7,1 9
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+
+double [,] NumbersArray = new double[rows, cols];
+    Random rand = new Random();
+   void PrintArray(double [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3} \t");
+        Console.WriteLine();
+    }
+}
+
+void FillArray (double [, ] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = Convert.ToDouble(rand.Next(-10, 100)/10.0);
+
+}       
+FillArray (NumbersArray);
+PrintArray(NumbersArray);
+
